d04d41f59ebb6e889162930e5c91787aa6e8ba2c
nishit435 <nishitkpatel19@gmail.com>
2025-02-03 23:49:05 +0530

    a

--- frontend/app/(ROOT)/page.jsx
+++ frontend/app/(ROOT)/page.jsx
@@ -1,14 +1,9 @@
-import { signIn } from "@/auth";
+import Navbar from "components/Navbar";
 
 export default function SignIn() {
   return (
-    <form
-      action={async () => {
-        "use server";
-        await signIn("google");
-      }}
-    >
-      <button type="submit">Signin with Google</button>
-    </form>
+    <>
+      <H1>HI</H1>
+    </>
   );
 }
